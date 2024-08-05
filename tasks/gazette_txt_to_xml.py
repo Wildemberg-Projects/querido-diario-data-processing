2c76c7f728d410556fb5c4c350633874ddc2d3c3
wildemberg-sales <junior_sales2010@hotmail.com>
2024-08-05 14:46:01 -0300

    Refatoração da lógica de busca de territorios/ano e geração de zips
    Co-authored-by: csafurtado <csafurtado.dev@gmail.com>

--- tasks/gazette_txt_to_xml.py
+++ tasks/gazette_txt_to_xml.py
@@ -8,64 +8,63 @@ from zipfile import ZipFile, ZIP_DEFLATED
 from pathlib import Path
 
 from .utils import hash_content
-from .interfaces import StorageInterface
+from .interfaces import StorageInterface,DatabaseInterface
 
 need_update_zip_state = False
 logger = logging.getLogger(__name__)
 
 
 
-def create_aggregates(database, storage):
+def create_aggregates(database:DatabaseInterface, storage:StorageInterface):
     """
     Create xml for all territories available in database
     """
     logger.info("Agregando os arquivos TXT para XML de territórios e estados...")
 
-    results_query_states = database.select("SELECT DISTINCT state_code FROM territories ORDER BY state_code ASC;")
+    results_query_states = list(database.select(f"SELECT t.state_code AS code, json_agg(json_build_object('id',t.id, 'name',t.name)) FROM territories t GROUP BY code;"))
 
-    for res in results_query_states:
-        
-        results_query_territories = database.select(f"SELECT * FROM territories WHERE state_code='{res[0]}';")
-
+    for state, territories_list in results_query_states:
         try:
-            create_aggregates_for_territories_and_states(list(results_query_territories), res[0], database, storage)
+            create_aggregates_for_territories_and_states(territories_list, state, database, storage)
         except:
             print(traceback.format_exc())
             continue
         
 
-def create_aggregates_for_territories_and_states(territories_list:list, state, database, storage):
+def create_aggregates_for_territories_and_states(territories_list:list, state:str, database:DatabaseInterface, storage:StorageInterface):
     """
     Create a .xml files for each year of gazettes for a territory  
     """
-    actual_year = datetime.now().year
-    base_year = 1960
+    
+    xml_files_dict = {}
 
-    for year in range(base_year, actual_year+1):
-        xml_files_arr = []
+    for territory in territories_list:
+        query_content_gazzetes_for_territory = list(database.select(f"""SELECT
+                                                                        date_part('Year', g.date) AS year,
+                                                                        json_agg(g.*)
+                                                                    FROM 
+                                                                        gazettes g 
+                                                                    WHERE
+                                                                        g.territory_id='{territory['id']}'
+                                                                    GROUP BY 
+                                                                        year
+                                                                    """
+                                                                    )) 
+        
 
-        for territories in territories_list:
+        for year, list_gazzetes_content in query_content_gazzetes_for_territory:
             root = ET.Element("root")
             xml_file = BytesIO()
 
-            gazzetes_query_content = list(database.select(f"SELECT * FROM gazettes\
-                                            WHERE territory_id='{territories[0]}' AND\
-                                            date BETWEEN '{year}-01-01' AND '{year}-12-31'\
-                                            ORDER BY date ASC;"))
-
-            if not gazzetes_query_content:
-                # logger.info(f"Nada encontrado para cidade {territories[1]}-{territories[2]} no ano {year}")
-                continue
-
-            print(f"Gerando XML para cidade {territories[1]}-{territories[2]} no ano {year}")
+            logger.info(f"Gerando XML para cidade {territory['name']}-{state} no ano {year}")
 
             meta_info_tag = ET.SubElement(root, "meta")
-            ET.SubElement(meta_info_tag, "uf").text = territories[2]
+            ET.SubElement(meta_info_tag, "uf").text = state
             ET.SubElement(meta_info_tag, "ano_publicacao").text = str(year)
-            ET.SubElement(meta_info_tag, "municipio").text = territories[1]
-            ET.SubElement(meta_info_tag, "municipio_codigo_ibge").text = territories[0]
+            ET.SubElement(meta_info_tag, "municipio").text = territory['name']
+            ET.SubElement(meta_info_tag, "municipio_codigo_ibge").text = territory['id']
 
-            xml_content_generate(gazzetes_query_content, root, territories[0], storage)
+            xml_content_generate(list_gazzetes_content, root, storage)
 
             # Format XML file
             xml_str = ET.tostring(root, encoding='unicode')
@@ -76,137 +75,141 @@ def create_aggregates_for_territories_and_states(territories_list:list, state, d
             xml_file.seek(0)
 
             meta_xml={
-                "xml":xml_file,
-                "territory_id":territories[0],
-                "state_code":territories[2],
+                "xml":xml_file.getvalue(),
+                "territory_id":territory['id'],
+                "state_code":state,
                 "year":year
             }
+            
+            if year not in xml_files_dict:
+                xml_files_dict[year] = []
+            xml_files_dict[year].append(meta_xml)
+            
+            xml_file.close()
+            
+            create_zip_for_territory(meta_xml, database, storage)
 
-            xml_files_arr.append(meta_xml)
+    # if need_update_zip_state:
+    create_zip_for_state(xml_files_dict, state, database, storage)
         
-        create_zip_for_territory(xml_files_arr, database, storage)
-
-        if need_update_zip_state:
-            create_zip_for_state(xml_files_arr, year, state, database, storage)
         
-        xml_file.close()
+        
 
 
-def create_zip_for_state(xml_arr:list, year, state_code, database, storage):
+def create_zip_for_state(xmls_years_dict:dict, state_code, database:DatabaseInterface, storage:StorageInterface):
     """
     Creating .zip files for the state with all its territories
     """
 
-    print(f"Gerando ZIP do estado {state_code} no ano {year}")
-    
-    zip_path = f"aggregates/{state_code}/{state_code}_{year}.zip"
-
-    zip_buffer = BytesIO()
-
-    with ZipFile(zip_buffer, 'w', ZIP_DEFLATED) as zip_file:
-        for xml_file in xml_arr:
-            zip_file.writestr(f"{xml_file['territory_id']}-{xml_file['year']}.xml", xml_file['xml'].getvalue())
+    for year in xmls_years_dict.keys():
+        print(f"Gerando ZIP do estado {state_code} no ano {year}")
         
-    zip_size = round(zip_buffer.getbuffer().nbytes / (1024 * 1024), 2)
-    zip_buffer.seek(0)
-    zip_buffer_copy = BytesIO(zip_buffer.getvalue())
-    zip_buffer_copy.seek(0)
-    storage.upload_content(zip_path, zip_buffer)
+        xmls = xmls_years_dict[year]
+        
+        zip_path = f"aggregates/{state_code}/{state_code}_{year}.zip"
 
-    hx = hash_content(zip_buffer_copy.read())
+        zip_buffer = BytesIO()
 
-    dict_query_info = {
-        "state_code" : state_code,
-        "territory_id" : None,
-        "file_path": zip_path,
-        "year": year,
-        "hash_info": hx,
-        "file_size_mb": zip_size,
-    }
-
-    query_existing_aggregate = list(database.select(f"SELECT hash_info FROM aggregates \
-                                                            WHERE file_path='{zip_path}';"))
-
-    if query_existing_aggregate and hx != query_existing_aggregate[0][0]:
-        database.insert("UPDATE aggregates SET \
-            state_code=%(state_code)s, last_updated=NOW(), \
-            hash_info=%(hash_info)s, file_size_mb=%(file_size_mb)s \
-            WHERE file_path=%(file_path)s;", dict_query_info)
-    else:
-        database.insert("INSERT INTO aggregates \
-                (territory_id, state_code, year, file_path, file_size_mb, hash_info, last_updated) \
-                VALUES (%(territory_id)s, %(state_code)s, %(year)s, %(file_path)s, %(file_size_mb)s, \
-                %(hash_info)s, NOW());", dict_query_info)
-
-    zip_buffer.close()
-
-
-def create_zip_for_territory(xml_arr:list, database, storage):
-    """
-    Creating .zip files for the year's territories 
-    """
-
-    global need_update_zip_state
-    need_update_zip_state = False
-
-    for xml_file in xml_arr:
-        try:
-            hx = hash_content(xml_file['xml'].getvalue().decode('utf-8'))
-            zip_path = f"aggregates/{xml_file['state_code']}/{xml_file['territory_id']}-{xml_file['year']}.zip"
+        with ZipFile(zip_buffer, 'w', ZIP_DEFLATED) as zip_file:
+            for xml_file in xmls:
+                zip_file.writestr(f"{xml_file['territory_id']}-{xml_file['year']}.xml", xml_file['xml'])
             
-            query_existing_aggregate = list(database.select(f"SELECT hash_info FROM aggregates \
-                                                WHERE file_path='{zip_path}';"))
-            
-            need_update = False
+        zip_size = round(zip_buffer.getbuffer().nbytes / (1024 * 1024), 2)
+        zip_buffer.seek(0)
+        zip_buffer_copy = BytesIO(zip_buffer.getvalue())
+        zip_buffer_copy.seek(0)
+        storage.upload_content(zip_path, zip_buffer)
 
-            if query_existing_aggregate:
-                need_update = hx != query_existing_aggregate[0][0]
-                if not need_update:
-                    continue
+        hx = hash_content(zip_buffer_copy.read())
 
-            need_update_zip_state = True
+        dict_query_info = {
+            "state_code" : state_code,
+            "territory_id" : None,
+            "file_path": zip_path,
+            "year": year,
+            "hash_info": hx,
+            "file_size_mb": zip_size,
+        }
 
-            zip_buffer = BytesIO()
+        query_existing_aggregate = list(database.select(f"SELECT hash_info FROM aggregates \
+                                                                WHERE file_path='{zip_path}';"))
 
-            with ZipFile(zip_buffer, 'w', ZIP_DEFLATED) as zip_file:
-                zip_file.writestr(f"{xml_file['territory_id']}-{xml_file['year']}.xml", xml_file['xml'].getvalue())
-            
-            zip_size = round(zip_buffer.tell() / (1024 * 1024), 2)
-            zip_buffer.seek(0)
-
-            storage.upload_content(zip_path, zip_buffer)
-
-            dict_query_info = {
-                "state_code" : xml_file['state_code'],
-                "territory_id" : xml_file['territory_id'],
-                "file_path": zip_path,
-                "year": xml_file['year'],
-                "hash_info": hx,
-                "file_size_mb": zip_size,
-            }
-
-            if need_update:
-                database.insert("UPDATE aggregates SET \
-                    state_code=%(state_code)s, last_updated=NOW(), \
-                    hash_info=%(hash_info)s, file_size_mb=%(file_size_mb)s \
-                    WHERE file_path=%(file_path)s;", dict_query_info)
-            else:
-                database.insert("INSERT INTO aggregates \
+        if query_existing_aggregate and hx != query_existing_aggregate[0][0]:
+            database.insert("UPDATE aggregates SET \
+                state_code=%(state_code)s, last_updated=NOW(), \
+                hash_info=%(hash_info)s, file_size_mb=%(file_size_mb)s \
+                WHERE file_path=%(file_path)s;", dict_query_info)
+        else:
+            database.insert("INSERT INTO aggregates \
                     (territory_id, state_code, year, file_path, file_size_mb, hash_info, last_updated) \
                     VALUES (%(territory_id)s, %(state_code)s, %(year)s, %(file_path)s, %(file_size_mb)s, \
                     %(hash_info)s, NOW());", dict_query_info)
 
-            zip_buffer.close()
-        except Exception as e:
-            print(traceback.format_exc())
+        zip_buffer.close()
 
 
-def xml_content_generate(gazzetes_query_content:list, root, territory_id : str, storage : StorageInterface):
+def create_zip_for_territory(xml_file:dict, database:DatabaseInterface, storage:StorageInterface):
+    """
+    Creating .zip files for the year's territories 
+    """
+
+    # global need_update_zip_state
+    # need_update_zip_state = False
+    try:
+        hx = hash_content(xml_file['xml'].decode('utf-8'))
+        zip_path = f"aggregates/{xml_file['state_code']}/{xml_file['territory_id']}-{xml_file['year']}.zip"
+        
+        query_existing_aggregate = list(database.select(f"SELECT hash_info FROM aggregates \
+                                            WHERE file_path='{zip_path}';"))
+        
+        need_update = False
+
+        if query_existing_aggregate:
+            need_update = hx != query_existing_aggregate[0][0]
+            if not need_update:
+                return
+
+        zip_buffer = BytesIO()
+
+        with ZipFile(zip_buffer, 'w', ZIP_DEFLATED) as zip_file:
+            zip_file.writestr(f"{xml_file['territory_id']}-{xml_file['year']}.xml", xml_file['xml'])
+        
+        zip_size = round(zip_buffer.tell() / (1024 * 1024), 2)
+        zip_buffer.seek(0)
+
+        storage.upload_content(zip_path, zip_buffer)
+
+        dict_query_info = {
+            "state_code" : xml_file['state_code'],
+            "territory_id" : xml_file['territory_id'],
+            "file_path": zip_path,
+            "year": xml_file['year'],
+            "hash_info": hx,
+            "file_size_mb": zip_size,
+        }
+
+        if need_update:
+            database.insert("UPDATE aggregates SET \
+                state_code=%(state_code)s, last_updated=NOW(), \
+                hash_info=%(hash_info)s, file_size_mb=%(file_size_mb)s \
+                WHERE file_path=%(file_path)s;", dict_query_info)
+        else:
+            database.insert("INSERT INTO aggregates \
+                (territory_id, state_code, year, file_path, file_size_mb, hash_info, last_updated) \
+                VALUES (%(territory_id)s, %(state_code)s, %(year)s, %(file_path)s, %(file_size_mb)s, \
+                %(hash_info)s, NOW());", dict_query_info)
+
+        zip_buffer.close()
+    except Exception as e:
+        print(traceback.format_exc())
+
+
+def xml_content_generate(list_gazzetes_content:list, root:ET.Element, storage : StorageInterface):
     all_gazettes_tag = ET.SubElement(root, "diarios")
 
-    for gazette in gazzetes_query_content:
+    for gazette in list_gazzetes_content:
         file_gazette_txt = BytesIO()
-        path_arq_bucket = Path(gazette[7])
+        path_arq_bucket = Path(gazette['file_path'])
 
         if path_arq_bucket.suffix == ".pdf":
             path_arq_bucket = path_arq_bucket.with_suffix(".txt")
@@ -215,20 +218,19 @@ def xml_content_generate(gazzetes_query_content:list, root, territory_id : str, 
         
         try:
             storage.get_file(path_arq_bucket, file_gazette_txt)
-
         except Exception as e:
-            print(f"Erro na obtenção do conteúdo de texto do diário do territorio {territory_id}: {e}")
+            print(f"Erro na obtenção do conteúdo de texto do diário do territorio {gazette['territory_id']}: {e}")
             file_gazette_txt.close()
 
             continue
 
         gazette_tag = ET.SubElement(all_gazettes_tag, "diario")
         meta_gazette = ET.SubElement(gazette_tag, "meta-diario")
-        ET.SubElement(meta_gazette, "url_arquivo_original").text = gazette[8]
-        ET.SubElement(meta_gazette, "poder").text = gazette[5]
-        ET.SubElement(meta_gazette, "edicao_extra").text = 'Sim' if gazette[4] else 'Não'
-        ET.SubElement(meta_gazette, "numero_edicao").text = str(gazette[3]) if str(gazette[3]) is not None else "Não há"
-        ET.SubElement(meta_gazette, "data_publicacao").text = datetime.strftime(gazette[2], "%d/%m")
+        ET.SubElement(meta_gazette, "url_arquivo_original").text = gazette['file_url']
+        ET.SubElement(meta_gazette, "poder").text = gazette['power']
+        ET.SubElement(meta_gazette, "edicao_extra").text = 'Sim' if gazette['is_extra_edition'] else 'Não'
+        ET.SubElement(meta_gazette, "numero_edicao").text = str(gazette['edition_number']) if str(gazette['edition_number']) is not None else "Não há"
+        ET.SubElement(meta_gazette, "data_publicacao").text = datetime.strftime((datetime.strptime(gazette['date'], "%Y-%m-%d").date()), "%d/%m")
         ET.SubElement(gazette_tag, "conteudo").text = file_gazette_txt.getvalue().decode('utf-8')
 
         file_gazette_txt.close()
